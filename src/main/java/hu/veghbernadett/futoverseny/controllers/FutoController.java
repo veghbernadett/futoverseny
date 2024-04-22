2c0682c0d8a850d16f672d3da82c33985ea235f8
bernadettvegh <veghbernadett@gmail.com>
2024-04-22 17:11:03 +0200

    Update Eredmeny repository and Futo

--- src/main/java/hu/veghbernadett/futoverseny/controllers/FutoController.java
+++ src/main/java/hu/veghbernadett/futoverseny/controllers/FutoController.java
@@ -1,15 +1,36 @@
 package hu.veghbernadett.futoverseny.controllers;
 
+import hu.veghbernadett.futoverseny.models.Eredmeny;
 import hu.veghbernadett.futoverseny.models.Futo;
+import hu.veghbernadett.futoverseny.repositories.EredmenyRepository;
 import hu.veghbernadett.futoverseny.repositories.FutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class FutoController {
 
     @Autowired
     private FutoRepository futoRepository;
+
+
+    // összes futó
+    @GetMapping("/getRunners")
+    public List<Futo> getRunners() {
+        return futoRepository.findAll();
+    }
+
+
+    // futó felvétele
+    @PostMapping(path = "/addRunner")
+    public Futo addRunner(@RequestBody Futo futo){
+        futoRepository.save(futo);
+        return futo;
+    }
+
 
     // TODO
     @GetMapping(path = "/futo")
@@ -24,11 +45,7 @@ public class FutoController {
 
     }
 
-    @PostMapping(path = "/futomentes")
-    public Futo addRunner(@RequestBody Futo futo){
-        futoRepository.save(futo);
-        return futo;
-    }
+
 
     //  TODO
     @GetMapping(path = "/futo/{id}")
--- src/main/java/hu/veghbernadett/futoverseny/repositories/EredmenyRepository.java
+++ src/main/java/hu/veghbernadett/futoverseny/repositories/EredmenyRepository.java
@@ -4,6 +4,9 @@ import hu.veghbernadett.futoverseny.models.Eredmeny;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EredmenyRepository extends JpaRepository<Eredmeny, Integer> {
+
 }
